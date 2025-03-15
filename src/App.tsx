032c7828c7b4eef402e3a68e8b4baf9e2c7e82de
Asdian <asdiank7@gmail.com>
2025-03-15 11:51:21 +0500

    delete test and welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import { WelcomePage } from "./pages/Welcome Page/WelcomePage";
 import { MainPage } from "./pages/Main Page/MainPage";
-import { Test } from "./pages/Test/Test";
 
 const routerConfig = createBrowserRouter([
   {
     path: "/",
-    element: <WelcomePage />,
-  },
-  {
-    path: "main-page",
     element: <MainPage />,
-  },
-  {
-    path: "test-page",
-    element: <Test />,
   },
 ]);
 
