dde8d876f9d2e16d06c8e6f7c0ae8b0004bf0ccc
Asdian <asdiank7@gmail.com>
2025-03-17 10:32:58 +0500

    add path to LecturePage to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { MainPage } from "./pages/Main Page/MainPage";
+import { LecturePage } from "./pages/Lecture Page/LecturePage";
 
 const routerConfig = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
+  },
+  {
+    path: "lecture-page",
+    element: <LecturePage />,
   },
 ]);
 
