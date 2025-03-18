f34f19594d3b31d96d38c5f66a7b4448b8a36725
Asdian <asdiank7@gmail.com>
2025-03-18 09:37:26 +0500

    main in Lecture Page have been created

--- src/pages/Lecture Page/LecturePage.style.ts
+++ src/pages/Lecture Page/LecturePage.style.ts
@@ -1,3 +1,61 @@
 import styled from "styled-components";
 
-export const StyleLecturePage = styled.div``;
+export const StyleLecturePage = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 100px;
+  margin: 100px;
+
+  img {
+    width: 660px;
+    border-radius: 20px;
+  }
+  .energetika {
+    display: flex;
+    gap: 80px;
+    align-items: center;
+  }
+  .smart-technology {
+    display: flex;
+    flex-direction: row-reverse;
+    gap: 80px;
+    align-items: center;
+  }
+  .life-save {
+    display: flex;
+    gap: 80px;
+    align-items: center;
+  }
+
+  .Info {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    h1 {
+      font-size: 48px;
+      font-weight: bold;
+      color: #333333;
+      font-family: "Inter", sans-serif;
+    }
+    h2 {
+      width: 400px;
+      font-size: 24;
+      font-weight: medium;
+      font-family: "Inter", sans-serif;
+      color: #888888;
+    }
+    button {
+      width: 108px;
+      height: 40px;
+      background-color: #222222;
+      color: #ffffff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 8px;
+      font-weight: bold;
+      font-family: "Inter", sans-serif;
+      font-size: 14px;
+    }
+  }
+`;
--- src/pages/Lecture Page/LecturePage.tsx
+++ src/pages/Lecture Page/LecturePage.tsx
@@ -3,7 +3,34 @@ import { StyleLecturePage } from "./LecturePage.style";
 export const LecturePage = () => {
   return (
     <StyleLecturePage>
-      <h1>Lecture Page</h1>
+      <div className="energetika">
+        <img src="./lecture_1.jpg" />
+        <div className="Info">
+          <h1>ЭНЕРГЕТИКА</h1>
+          <h2>
+            Install the plugin and convert your designs to a responsive site.
+          </h2>
+          <button>Get started</button>
+        </div>
+      </div>
+      <div className="smart-technology">
+        <img src="./lecture_2.jpg" />
+        <div className="Info">
+          <h1>SMART TECHNOLOGY</h1>
+          <h2>Browse dozens of templates. Click, duplicate, customize.</h2>
+          <button>Get started</button>
+        </div>
+      </div>
+      <div className="life-save">
+        <img src="./image_3.jpg" />
+        <div className="Info">
+          <h1>БЕЗОПАСНОСТЬ ЖИЗНЕДЕЯТЕЛЬНОСТИ</h1>
+          <h2>
+            Install the plugin and convert your designs to a responsive site.
+          </h2>
+          <button>Get started</button>
+        </div>
+      </div>
     </StyleLecturePage>
   );
 };
